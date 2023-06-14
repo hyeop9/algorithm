954aa1fbc56247166c98dbf5ba479e9726c22ed0
Hyunyeop Ahn <hyeop9@gmail.com>
2023-06-14 13:51:34 +0900

    refactor : fibonacci code

--- fibonacci/main.c
+++ fibonacci/main.c
@@ -1,55 +1,59 @@
 #include <stdio.h>
 #include <time.h>
 
-// 재귀 함수
-unsigned long long recursiveFibonacci(int n)
-{
-    if (n <= 1)
+// 재귀 함수를 사용하여 피보나치 수 계산
+long long fibonacciRecursive(int n) {
+    // 기저 조건: n이 0 또는 1일 때는 n을 반환
+    if (n == 0 || n == 1) {
         return n;
-    else
-        return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+    }
+
+    // 피보나치 수 계산
+    return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
 }
 
-// 동적 프로그래밍
-unsigned long long dynamicFibonacci(int n)
-{
-    unsigned long long fib[n + 1];
+// 동적 프로그래밍을 사용하여 피보나치 수 계산
+long long fibonacciDynamic(int n) {
+    // 피보나치 수를 저장할 배열
+    long long fib[n + 1];
     int i;
 
-    fib[0] = 0;
-    fib[1] = 1;
+    // 초기 값 설정
+    fib[0] = 0;  // 첫 번째 피보나치 수
+    fib[1] = 1;  // 두 번째 피보나치 수
 
-    for (i = 2; i <= n; i++)
-        fib[i] = fib[i - 1] + fib[i - 2];
+    // 피보나치 수 계산
+    for (i = 2; i <= n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];  // 현재 항은 이전 두 항의 합으로 계산됨
+    }
 
+    // n번째 피보나치 수 반환
     return fib[n];
 }
 
-int main()
-{
+int main() {
     int n;
-    clock_t start, end;
-    double recursiveTime, dynamicTime;
-    unsigned long long recursiveResult, dynamicResult;
 
     printf("Input: ");
     scanf("%d", &n);
 
-    // 재귀 함수 실행 및 수행 시간 측정
-    start = clock();
-    recursiveResult = recursiveFibonacci(n);
-    end = clock();
-    recursiveTime = ((double)(end - start)) / CLOCKS_PER_SEC;
+    // 재귀 함수를 사용하여 피보나치 수 계산 및 수행 시간 측정
+    clock_t startRecursive = clock();
+    long long resultRecursive = fibonacciRecursive(n);
+    clock_t endRecursive = clock();
 
-    // 동적 프로그래밍 실행 및 수행 시간 측정
-    start = clock();
-    dynamicResult = dynamicFibonacci(n);
-    end = clock();
-    dynamicTime = ((double)(end - start)) / CLOCKS_PER_SEC;
+    // 동적 프로그래밍을 사용하여 피보나치 수 계산 및 수행 시간 측정
+    clock_t startDynamic = clock();
+    long long resultDynamic = fibonacciDynamic(n);
+    clock_t endDynamic = clock();
+
+    // 수행 시간 계산
+    double timeRecursive = (double)(endRecursive - startRecursive) * 1000.0 / CLOCKS_PER_SEC;
+    double timeDynamic = (double)(endDynamic - startDynamic) * 1000.0 / CLOCKS_PER_SEC;
 
     // 결과 출력
-    printf("Recursive: %llu / %.3lfsec\n", recursiveResult, recursiveTime);
-    printf("Dynamic: %llu / %.3lfsec\n", dynamicResult, dynamicTime);
+    printf("Recursive: %lld / %.3f msec\n", resultRecursive, timeRecursive);
+    printf("Dynamic: %lld / %.3f msec\n", resultDynamic, timeDynamic);
 
     return 0;
 }
